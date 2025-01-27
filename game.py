272f1f2543dd1610ec94a4291aa397e3ee136f38
NattalZ30 <nattalz@yahoo.com>
2025-01-27 22:04:18 +0000

    game-display-setup

--- game.py
+++ game.py
@@ -1,3 +1,32 @@
+# imports
+import math, random, sys, os
 import pygame
+from pygame.locals import *
 
-print("meow")
+#event loop
+def events():
+    for event in pygame.event.get(): 
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+#define display surface
+W, H = 600, 400
+HW, HH = W/2, H/2
+WINDOW_SIZE = (W, H)
+
+#display initiation
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode(WINDOW_SIZE) 
+##display = pygame.Surface((HW, HH))
+pygame.display.set_caption('RL Pygame Platform Game')
+
+#game loop
+while True: 
+    pygame.display.update()
+    clock.tick(60)
+
+    events()
+    
+
