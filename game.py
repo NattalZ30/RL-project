aebf97ba9bfbc807e1a0e92d16d9df2c4c6f1dbd
NattalZ30 <nattalz@yahoo.com>
2025-02-15 13:33:22 +0000

    format+class

--- game.py
+++ game.py
@@ -3,13 +3,6 @@ import math, random, sys, os
 import pygame
 from pygame.locals import *
 
-#event loop
-def events():
-    for event in pygame.event.get(): 
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
 #define display surface
 W, H = 600, 400
 HW, HH = W/2, H/2
@@ -22,6 +15,37 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 ##display = pygame.Surface((HW, HH))
 pygame.display.set_caption('RL Pygame Platform Game')
 
+# FUNCTIONS -------------------------------------------------------
+#event loop
+def events():
+    for event in pygame.event.get(): 
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+class player:
+    def __init__(self, velocity, maxJump):
+        self.velocity = velocity
+        self.maxJump = maxJump
+
+    def setLocation(self, x, y):
+        self.x = x
+        self.y = y
+        self.xVel = 6-6
+        self.jump = False
+        self.jumpCount = 6-6
+        self.falling = True
+
+    def keys(self):
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
+
+# MAIN ------------------------------------------------------------
 #game loop
 while True: 
     pygame.display.update()
