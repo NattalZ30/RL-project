a6a23681bd6b40c5b9db9c1923271ecf0bd0976f
NattalZ30 <nattalz@yahoo.com>
2025-02-15 14:34:25 +0000

    player-class

--- game.py
+++ game.py
@@ -23,27 +23,79 @@ def events():
             pygame.quit()
             sys.exit()
 
-class player:
-    def __init__(self, velocity, maxJump):
-        self.velocity = velocity
-        self.maxJump = maxJump
+#collision checker
+def collision_test(rect,tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
 
-    def setLocation(self, x, y):
+# CLASSES -------------------------------------------------------
+class player:
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
-        self.xVel = 6-6
-        self.jump = False
-        self.jumpCount = 6-6
-        self.falling = True
+        self.width = width
+        self.height = height
+        self.player_movement = [0,0]
+        self.moving_left = False
+        self.moving_right = False
+        self.air_timer = 0
+        self.vertical_momentum = 0
+        self.jumping = False
+        self.collision_types = {'top':False,'bottom':False,'right':False,'left':False}
 
     def keys(self):
-        pass
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                self.moving_right = True
+            if event.key == K_LEFT:
+                self.moving_left = True
+            if event.key == K_UP or event.key == K_SPACE:
+                self.jumping = True
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                self.moving_right = False
+            if event.key == K_LEFT:
+                self.moving_left = False
+            if event.key == K_UP or event.key == K_SPACE:
+                self.jumping = False
 
     def move(self):
-        pass
+        if self.moving_right == True and self.collision_types.right == False:
+            self.player_movement[0] += 2
+        if self.moving_left == True and self.collision_types.left == False:
+            self.player_movement[0] -= 2
+        if self.collision_types['bottom'] == True:
+            self.air_timer = 0
+            self.vertical_momentum = 0
+        else:
+            self.air_timer += 1
+    
+    def collisions(self,rect,tiles):
+        rect.x += self.player_movement[0]
+        hit_list = collision_test(rect,tiles)
+        for tile in hit_list:
+            if self.player_movement[0] > 0:
+                rect.right = tile.left
+                self.collision_types['right'] = True
+            elif self.player_movement[0] < 0:
+                rect.left = tile.right
+                self.collision_types['left'] = True
+        rect.y += self.player_movement[1]
+        hit_list = collision_test(rect,tiles)
+        for tile in hit_list:
+            if self.player_movement[1] > 0:
+                rect.bottom = tile.top
+                self.collision_types['bottom'] = True
+            elif self.player_movement[1] < 0:
+                rect.top = tile.bottom
+                self.collision_types['top'] = True
+        return rect, self.collision_types
 
     def draw(self):
-        pass
+        self.player_rect = pygame.Rect(self.x,self.y,self.width,self.height)
 
 # MAIN ------------------------------------------------------------
 #game loop
@@ -51,6 +103,9 @@ while True:
     pygame.display.update()
     clock.tick(60)
 
-    events()
+    for event in pygame.event.get(): 
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
     
 
